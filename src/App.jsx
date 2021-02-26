2e0fe0f1882783551597e3a63cabe4918fa6a195
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-26 08:25:57 -0600

    added test files

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import Test from "./components/Test";
 const App = () => {
     return (
         <div>
-            <Test />
+            <Test 
+                stringTest="Gabe" 
+                numberTest={33} 
+                booleanTest={false} 
+                undefinedTest={undefined}
+                undefinedTest={null} 
+                NaNTest={NaN}
+                arrayTest={["a", "b", "c"]}
+                objectTest={{ name: "Gabe", age: 38 }}
+            />
         </div>
     );
 }
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Test = (props) => {
     return (
         <div>
-            <h1>Test</h1>
+            <h1>Testing prop string type: {props.stringTest}</h1>
+            <h1>Testing prop number type: {props.numberTest}</h1>
+            <h1>Testing prop boolean type: {props.booleanTest}</h1>
+            <h1>Testing prop undefined type: {props.undefinedTest}</h1>
+            <h1>Testing prop null type: {props.nullTest}</h1>
+            <h1>Testing prop NaN type: {props.NaNTest}</h1>
+            <h1>Testing prop array type: {props.arrayTest}</h1>
+            <h1>Testing prop object type: {props.objectTest.name}</h1>
         </div>
     );
 }
